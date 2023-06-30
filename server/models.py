b919f207c2e99fff2281641221a48fb0ae7907ab
Erickkirui <erickkirui653@gmail.com>
2023-06-30 15:18:58 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -9,6 +9,13 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError('Phone number must be exactly ten digits.')
+        return phone_number
+    
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
@@ -21,9 +28,31 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+
     content = db.Column(db.String)
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be at least 250 characters long.')
+        return content
+
     category = db.Column(db.String)
+
+    @validates('category')
+    def validate_category(self, key, category):
+        allowed_categories = ['Fiction', 'Non-Fiction']
+        if category not in allowed_categories:
+            raise ValueError('Invalid category. Must be either Fiction or Non-Fiction.')
+        return category
+    
     summary = db.Column(db.String)
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary must be a maximum of 250 characters.')
+        return summary
+    
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
